90c168cd68843e99d7000e0c283f2d63a5113f13
Jwan Hasan <Cloudcaelum22@outlook.com>
2026-02-04 20:24:03 +0100

    creating dto

---
+++ Shared/Dtos/AlbumDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Dtos
+{
+    internal class AlbumDto
+    {
+        public required string Name { get; set; }
+
+        public int ArtistId { get; set; }
+    }
+}
---
+++ Shared/Dtos/SongDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Dtos
+{
+    public class SongDto
+    {
+        public required string Name { get; set; }
+
+        public int ArtistId { get; set; }
+        public int AlbumsId { get; set; }
+
+
+    }
+}
