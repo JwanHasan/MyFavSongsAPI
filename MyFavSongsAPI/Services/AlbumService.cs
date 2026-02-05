3562900fb0885c80d9d891bd2262ab6b017bfcc0
Jwan Hasan <Cloudcaelum22@outlook.com>
2026-02-05 13:45:52 +0100

    link interface with service

--- MyFavSongsAPI/Services/AlbumService.cs
+++ MyFavSongsAPI/Services/AlbumService.cs
@@ -1,6 +1,32 @@
-﻿namespace MyFavSongsAPI.Services
+﻿using Shared.Entities;
+
+namespace MyFavSongsAPI.Services
 {
-    public class AlbumService
+    public class AlbumService : IAlbumService
     {
+        public Task<Album> CreateAlbumAsync(string name, int artistId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Album> DeleteAlbumAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Album> GetAlbumByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Album> GetAllAlbumAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Album> UpdateAlbumAsync(int id, Album album)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
