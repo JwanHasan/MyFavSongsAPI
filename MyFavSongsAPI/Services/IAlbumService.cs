6efd87d22038daed06e8af7a87f1737f2b244ec1
Jwan Hasan <Cloudcaelum22@outlook.com>
2026-02-05 13:44:44 +0100

    adding album interface

--- MyFavSongsAPI/Services/IAlbumService.cs
+++ MyFavSongsAPI/Services/IAlbumService.cs
@@ -1,6 +1,14 @@
-﻿namespace MyFavSongsAPI.Services
+﻿using Shared.Entities;
+
+namespace MyFavSongsAPI.Services
 {
     public interface IAlbumService
     {
+        Task<Album> CreateAlbumAsync(string name, int artistId);
+        
+        Task<Album> UpdateAlbumAsync(int id , Album album);
+        Task<Album> DeleteAlbumAsync(int id);
+        Task<Album> GetAllAlbumAsync();
+        Task<Album> GetAlbumByIdAsync(int id);
     }
 }
